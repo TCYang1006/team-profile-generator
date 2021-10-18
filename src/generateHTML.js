c129f0b49aa8d165fc3114b89f334c450161cf53
Peter Thao <peterthao@Peters-iMac.local>
2021-10-17 22:26:20 -0400

    minor tweet on hmtl

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,7 +5,7 @@ const generateManager = function (Manager) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${Manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <h4><i class="fas fa-mug-hot"></i> Manager</h4>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${Manager.id}</p>
@@ -24,7 +24,7 @@ const generateEngineer = function (Engineer) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${Engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                <h4><i class="fas fa-glasses"></i> Engineer</h4>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${Engineer.id}</p>
@@ -43,7 +43,7 @@ const generateIntern = function (Intern) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${Intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+                <h4><i class="fas fa-user-graduate"></i> Intern</h4>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${Intern.id}</p>
@@ -110,14 +110,13 @@ module.exports.getHTML = (teamData) =>{
         <title>Team Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <header>
             <nav class="navbar" id="navbar">
-                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+                <span class="navbar-brand mb-0 h1 w-100 text-center" style="background-color: red; color: white" id="navbar-text">My Team</span>
             </nav>
         </header>
         <main>
@@ -130,9 +129,8 @@ module.exports.getHTML = (teamData) =>{
         </main>
         
     </body>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     </html>
   `;
 }; 
